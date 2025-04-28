027c5c1211e24d1c9464faaf074e4afbd0ef15e9
MarceloCarabajal <marce.tsgia@gmail.com>
2025-04-28 11:45:49 -0300

    after implementacion de redux y base de datos al proyecto

---
+++ afterclass-4/ecommerce/src/store/index.js
@@ -0,0 +1,21 @@
+import { setupListeners } from "@reduxjs/toolkit/query";
+import counterReducer from "../features/Counter/counterSlice";
+import shopReducer from "../features/Shop/shopSlice";
+import cartReducer from "../features/Cart/cartSlice";
+import { shopApi } from "../services/shopServices";
+
+const { configureStore } = require("@reduxjs/toolkit");
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+        shop: shopReducer,
+        cart: cartReducer,
+        [shopApi.reducerPath]: shopApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shopApi.middleware)
+})
+
+setupListeners(store.dispatch)
+
+export default store;
